c577e44f94894bf6a4ac55adb2bf30181c8ac620
haavardj <haavardj@cs.uit.no>
2018-02-20 11:11:20 +0100

    feat: add stdout and io logger setup

--- logger/logger.go
+++ logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -28,6 +29,30 @@ func CreateLogger(hostName string, logName string) *Log {
 	errLog := log.New(logFile, errPrefix, log.Lshortfile)
 	infoLog := log.New(logFile, infoPrefix, log.Lshortfile)
 	debugLog := log.New(logFile, debugPrefix, log.Lshortfile)
+
+	return &Log{Err: errLog, Info: infoLog, Debug: debugLog}
+}
+
+func CreateStdOutLogger(hostName string, logName string) *Log {
+	errPrefix := fmt.Sprintf("\x1b[31m %s \x1b[0m", hostName)
+	infoPrefix := fmt.Sprintf("\x1b[32m %s \x1b[0m", hostName)
+	debugPrefix := fmt.Sprintf("\x1b[33m %s \x1b[0m", hostName)
+
+	errLog := log.New(os.Stdout, errPrefix, log.Lshortfile)
+	infoLog := log.New(os.Stdout, infoPrefix, log.Lshortfile)
+	debugLog := log.New(os.Stdout, debugPrefix, log.Lshortfile)
+
+	return &Log{Err: errLog, Info: infoLog, Debug: debugLog}
+}
+
+func CreateIOLogger(out io.Writer, hostName string, logName string) *Log {
+	errPrefix := fmt.Sprintf("\x1b[31m %s \x1b[0m", hostName)
+	infoPrefix := fmt.Sprintf("\x1b[32m %s \x1b[0m", hostName)
+	debugPrefix := fmt.Sprintf("\x1b[33m %s \x1b[0m", hostName)
+
+	errLog := log.New(out, errPrefix, log.Lshortfile)
+	infoLog := log.New(out, infoPrefix, log.Lshortfile)
+	debugLog := log.New(out, debugPrefix, log.Lshortfile)
 
 	return &Log{Err: errLog, Info: infoLog, Debug: debugLog}
 }
