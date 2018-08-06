62da890315f0ff4ec1bd6a6e349a0dfc6834030f
jon foss mikalsen <jon_fossen94@hotmail.com>
2018-08-06 14:03:06 +0200

    test: first node test draft.

--- core/node_test.go
+++ core/node_test.go
@@ -1,7 +1,17 @@
 package core
 
 import (
+	"crypto/tls"
+	"os"
+	"testing"
+
+	log "github.com/inconshreveable/log15"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"golang.org/x/net/context"
+
+	"github.com/joonnna/ifrit/protobuf"
 )
 
 // Define the suite, and absorb the built-in basic suite
@@ -9,65 +19,7 @@ import (
 // returns the current testing context
 type NodeTestSuite struct {
 	suite.Suite
-	*Node
-}
-
-/*
-func newTestPeer(id string, numRings uint32, addr string) (*peer, *ecdsa.PrivateKey) {
-	var i, mask uint32
-
-	peerPrivKey, err := genKeys()
-	if err != nil {
-		panic(err)
-	}
-
-	p := &peer{
-		publicKey:   &peerPrivKey.PublicKey,
-		peerId:      newPeerId([]byte(id)),
-		addr:        addr,
-		accusations: make(map[uint32]*accusation),
-	}
-
-	for i = 1; i <= numRings; i++ {
-		p.accusations[i] = nil
-	}
-
-	for i = 0; i < numRings; i++ {
-		mask = setBit(mask, i)
-	}
-
-	localNote := &note{
-		epoch:  1,
-		mask:   mask,
-		peerId: p.peerId,
-	}
-
-	_, err = localNote.signAndMarshal(peerPrivKey)
-	if err != nil {
-		panic(err)
-	}
-
-	p.recentNote = localNote
-
-	return p, peerPrivKey
-}
-
-func (suite *NodeTestSuite) SetupTest() {
-	var numRings uint32 = 6
-
-	p, priv := newTestPeer("mainPeer1234", numRings, "localhost:1000")
-
-	v, err := newView(numRings, p.peerId, p.addr)
-	require.NoError(suite.T(), err, "Failed to create view")
-
-	n := &Node{
-		peer:     p,
-		privKey:  priv,
-		view:     v,
-		protocol: correct{},
-	}
-
-	suite.Node = n
+	nodes []*Node
 }
 
 func TestNodeTestSuite(t *testing.T) {
@@ -75,6 +27,76 @@ func TestNodeTestSuite(t *testing.T) {
 
 	r.SetHandler(log.CallerFileHandler(log.StreamHandler(os.Stdout, log.TerminalFormat())))
 
+	viper.Set("use_ca", false)
+	viper.Set("use_viz", false)
+
 	suite.Run(t, new(NodeTestSuite))
 }
-*/
+
+func (suite *NodeTestSuite) SetupTest() {
+	for i := 0; i < 30; i++ {
+		n, err := NewNode(&clientStub{}, &serverStub{})
+		require.NoError(suite.T(), err, "Failed to create node.")
+
+		suite.nodes = append(suite.nodes, n)
+	}
+}
+
+func (suite *NodeTestSuite) TestGossip() {
+	// Everyone gossips with everyone,
+	// then assert that everyone has the same view.
+
+	for _, n := range suite.nodes {
+		for _, n2 := range suite.nodes {
+			reply, err := n2.Spread(context.Background(), n.collectGossipContent())
+			if err != nil {
+				continue
+			}
+			n.mergeCertificates(reply.GetCertificates())
+			n.mergeNotes(reply.GetNotes())
+			n.mergeAccusations(reply.GetAccusations())
+		}
+	}
+
+	for _, n := range suite.nodes {
+		for _, n2 := range suite.nodes {
+			require.NoError(suite.T(), n.view.Compare(n2.view), "Views are not equal.")
+		}
+	}
+
+}
+
+type clientStub struct {
+}
+
+func (cs *clientStub) Init(config *tls.Config) {
+}
+
+func (cs *clientStub) Gossip(addr string, args *gossip.State) (*gossip.StateResponse, error) {
+	return nil, nil
+}
+
+func (cs *clientStub) SendMsg(addr string, args *gossip.Msg) (*gossip.MsgResponse, error) {
+	return nil, nil
+}
+
+func (cs *clientStub) CloseConn(addr string) {
+}
+
+type serverStub struct {
+}
+
+func (ss *serverStub) Init(config *tls.Config, n interface{}, maxConcurrent uint32) error {
+	return nil
+}
+
+func (ss *serverStub) Addr() string {
+	return "127.0.0.1:8000"
+}
+
+func (ss *serverStub) Start() error {
+	return nil
+}
+
+func (ss *serverStub) ShutDown() {
+}
