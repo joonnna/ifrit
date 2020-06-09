51a58b4a639c3b72f14256c92d8d1e16d82dfcc6
haavardj <havard.johansen@uit.no>
2020-06-09 12:52:53 +0200

    add configor

--- cmd/ca/main.go
+++ cmd/ca/main.go
@@ -8,8 +8,20 @@ import (
 	"syscall"
 
 	log "github.com/inconshreveable/log15"
+	"github.com/jinzhu/configor"
 	"github.com/joonnna/ifrit/cauth"
 )
+
+var Config = struct {
+	Name         string `default:"Ifrit Test Groups"`
+	Version      string `default:"1.0.0"`
+	Host         string `default:""`
+	Port         uint32 `default:"8080"`
+	PathName     string `default:"./cadir"`
+	NumRings     uint32 `default:"12"`
+	NumBootNodes uint32 `default:"0"`
+	LogFile      string `default:"./ifritd.log"`
+}{}
 
 // saveState saves ca private key and public certificates to disk.
 func saveState(ca *cauth.Ca) {
@@ -25,19 +37,21 @@ func saveState(ca *cauth.Ca) {
 }
 
 func main() {
-	var logfile string
+
 	var h log.Handler
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
+	configor.New(&configor.Config{Debug: true}).Load(&Config, "ca-config.yml")
+
 	args := flag.NewFlagSet("args", flag.ExitOnError)
-	args.StringVar(&logfile, "logfile", "", "Log to file.")
+	args.StringVar(&Config.LogFile, "logfile", "", "Log to file.")
 	args.Parse(os.Args[1:])
 
 	r := log.Root()
 
 	if logfile != "" {
-		h = log.CallerFileHandler(log.Must.FileHandler(logfile, log.LogfmtFormat()))
+		h = log.CallerFileHandler(log.Must.FileHandler(Config.LogFile, log.LogfmtFormat()))
 	} else {
 		h = log.StreamHandler(os.Stdout, log.LogfmtFormat())
 	}
